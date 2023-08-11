32a5cf6d8885bbdb05f50f6f47f6a7b4d08188b7
Elian-beep <elianoliver101@gmail.com>
2023-08-10 23:12:17 -0300

    :rocket: ci: create scripts migrations

--- src/database/migrations/1691715921842-CreateStudent.ts
+++ src/database/migrations/1691715921842-CreateStudent.ts
@@ -1,11 +1,38 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
 
 export class CreateStudent1691715921842 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "student",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },{
+                        name: "name",
+                        type: "varchar",
+                    },{
+                        name: "user",
+                        type: "varchar"
+                    },{
+                        name: "password",
+                        type: "varchar",
+                        length: "16"
+                    },{
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("student");
     }
 
 }
